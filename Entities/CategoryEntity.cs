f47e206d25f823e42fe707ea16eb10929146ea78
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-05-23 15:01:55 -0300

    agregue los atributos necesarios y el datanotacion

--- Entities/CategoryEntity.cs
+++ Entities/CategoryEntity.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities
 {
-    internal class CategoryEntity
+    public class CategoryEntity
     {
+        [Key]
+        [StringLength(50)]
+        public required string CategoryId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public required string CategoryName { get; set; }
+
     }
 }
