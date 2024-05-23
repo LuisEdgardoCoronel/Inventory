8bdadbe05a92e33ed2dd72c586a0a33d55727012
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-05-23 19:23:53 -0300

    cree el context

---
+++ DataAccess/InventaryContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class InventaryContext:DbContext
+    {
+        public DbSet<ProductEntity> TProducts { get; set; }
+        public DbSet<CategoryEntity> TCategories { get; set; }
+        public DbSet<StorageEntity> TStorages { get; set; }
+        public DbSet<InputOutputEntity> TInputs { get; set; }
+        public DbSet<WareHouseEntity> TWareHouse { get; set; }
+    }
+}
