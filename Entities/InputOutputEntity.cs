27ca325739689f73db5dc57aa8bcb8e11550eb38
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-05-23 16:51:24 -0300

    clase de entradas y salidas

---
+++ Entities/InputOutputEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class InputOutputEntity
+    {
+        [Key, StringLength(50)]
+        public required string InOutId { get; set; }
+
+        [Required]
+        public DateTime InOutDate { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public bool IsInput { get; set; }
+    }
+}
