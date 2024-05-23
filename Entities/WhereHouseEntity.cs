a8b48184df6a804a84b1dae803a93bfb032b8be4
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-05-23 16:50:35 -0300

    clase para las bodegas

---
+++ Entities/WhereHouseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class WhereHouseEntity
+    {
+        [Key]
+        [StringLength(50)]
+        public required string WhereHouseId {  get; set; }
+
+        [Required,StringLength(100)]
+        public required string WhereHouseName { get; set; }
+
+        [Required,StringLength(100)]
+        public required string WhereHouseAddress { get; set; }
+    }
+}
