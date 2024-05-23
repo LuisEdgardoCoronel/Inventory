ffc1582f89813c9ddd64c55ce7cdaeb57bdaa81e
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-05-23 16:51:04 -0300

    clase de productos

---
+++ Entities/ProductEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class ProductEntity
+    {
+        [Key]
+        [StringLength(10)]
+        public required string ProductId { get; set; }
+
+        [Required, StringLength(100)]
+        public required string ProductName { get; set; }
+
+        [StringLength(600)]
+        public string ProductDescription { get; set; }= string.Empty;
+
+        public int TotalQuantity { get; set; }
+    }
+}
