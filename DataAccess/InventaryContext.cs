d9ab3ca4b0a45bca299d6c8600c7a4952ffee5cf
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-05-24 15:53:22 -0300

    cree el modelo para el contexto

--- DataAccess/InventaryContext.cs
+++ DataAccess/InventaryContext.cs
@@ -20,5 +20,40 @@ namespace DataAccess
                 optionsBuilder.UseSqlServer("Data Source=MI-PC\\SQLEXPRESS; Initial Catalog=InventoryDb; user id= '';password='';TrustServerCertificate=True;Integrated Security=True");
             }
         }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //creando entidades
+            modelBuilder.Entity<CategoryEntity>().HasData(
+                new CategoryEntity { CategoryId="ASH", CategoryName="Aseo Hogar"},
+                new CategoryEntity { CategoryId="ASP", CategoryName="Aseo Personal"},
+                new CategoryEntity { CategoryId = "HGR", CategoryName="Hogar"},
+                new CategoryEntity { CategoryId="PRF", CategoryName="Perfumeria"}
+                );
+
+            modelBuilder.Entity<WareHouseEntity>().HasData(
+                new WareHouseEntity
+                {
+                    WareHouseId = Guid.NewGuid().ToString(),
+                    WareHouseName = "Bodega Central",
+                    WareHouseAddress = "Sarmiento 345"
+                },
+                new WareHouseEntity
+                {
+                    WareHouseId = Guid.NewGuid().ToString(),
+                    WareHouseName = "Bodega Norte",
+                    WareHouseAddress = "Av. Libertador 1345"
+                },
+                new WareHouseEntity
+                {
+                    WareHouseId = Guid.NewGuid().ToString(),
+                    WareHouseName = "Bodega Sur",
+                    WareHouseAddress = "24 de Septiembre y Ayacucho"
+                }
+                );
+        }
     }
 }
