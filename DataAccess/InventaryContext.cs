b3870d2fa703efac857e5458f5b7d95553f03db6
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-05-23 21:54:24 -0300

    agregue la configuracion para conectar con la base de datos

--- DataAccess/InventaryContext.cs
+++ DataAccess/InventaryContext.cs
@@ -1,19 +1,24 @@
 ﻿using Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccess
 {
-    public class InventaryContext:DbContext
+    public class InventaryContext : DbContext
     {
         public DbSet<ProductEntity> TProducts { get; set; }
         public DbSet<CategoryEntity> TCategories { get; set; }
         public DbSet<StorageEntity> TStorages { get; set; }
         public DbSet<InputOutputEntity> TInputs { get; set; }
         public DbSet<WareHouseEntity> TWareHouse { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=MI-PC\\SQLEXPRESS; Initial Catalog=InventoryDb; user id= '';password='';TrustServerCertificate=True;Integrated Security=True");
+            }
+        }
     }
 }
